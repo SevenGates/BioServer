49b6076cddeb85c04857786c7d004a779555683c
SevenGates <alexander.hult14@gmail.com>
2017-03-06 13:50:55 +0100

    Initial Commit

---
+++ BioServer/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BioServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncSocketListener server = new AsyncSocketListener();
+            server.StartListening();
+        }
+    }
+}
